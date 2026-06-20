547fcabeea3342753dab117f4a2cd44fcfae4b26
DrenTey <stemicha@gmail.com>
2026-06-20 18:08:15 +0200

    Onl1
    Pierwsze testowe źródło danych

---
+++ onl1.js
@@ -0,0 +1,571 @@
+const onl1a = [
+	{
+		"id": "sienkiewiczCzlowiekMysliNowozytnej2008g",
+		"type": "article-journal",
+		"call-number": "ONL CTO 2008/1",
+		"citation-key": "sienkiewiczCzlowiekMysliNowozytnej2008g",
+		"container-title": "Colloquia Theologica Ottoniana",
+		"issue": "1",
+		"journalAbbreviation": "CTO",
+		"note": "tex.archive-location: Bazhum#530/issues/47620",
+		"page": "7 - 25",
+		"title": "Człowiek w myśli nowożytnej",
+		"author": [
+			{
+				"family": "Sienkiewicz",
+				"given": "Edward"
+			}
+		],
+		"issued": {
+			"date-parts": [
+				[
+					"2008"
+				]
+			]
+		}
+	},
+	{
+		"id": "poplawskiKlasycznaKoncepcjaOntologiczna2008a",
+		"type": "article-journal",
+		"call-number": "ONL CTO 2008/1",
+		"citation-key": "poplawskiKlasycznaKoncepcjaOntologiczna2008a",
+		"container-title": "Colloquia Theologica Ottoniana",
+		"issue": "1",
+		"journalAbbreviation": "CTO",
+		"note": "tex.archive-location: Bazhum#530/issues/47620",
+		"page": "27 - 40",
+		"title": "Klasyczna koncepcja ontologiczna a ontologia relacyjna",
+		"author": [
+			{
+				"family": "Popławski",
+				"given": "Jarosław"
+			}
+		],
+		"issued": {
+			"date-parts": [
+				[
+					"2008"
+				]
+			]
+		}
+	},
+	{
+		"id": "waleszczukJosephaHoffneraRozumienie2008a",
+		"type": "article-journal",
+		"call-number": "ONL CTO 2008/1",
+		"citation-key": "waleszczukJosephaHoffneraRozumienie2008a",
+		"container-title": "Colloquia Theologica Ottoniana",
+		"issue": "1",
+		"journalAbbreviation": "CTO",
+		"note": "tex.archive-location: Bazhum#530/issues/47620",
+		"page": "41 - 55",
+		"title": "Josepha Höffnera rozumienie katolickiej nauki społecznej",
+		"author": [
+			{
+				"family": "Waleszczuk",
+				"given": "Zbigniew"
+			}
+		],
+		"issued": {
+			"date-parts": [
+				[
+					"2008"
+				]
+			]
+		}
+	},
+	{
+		"id": "harasimiakSystemRepresjiKarnych2008a",
+		"type": "article-journal",
+		"call-number": "ONL CTO 2008/1",
+		"citation-key": "harasimiakSystemRepresjiKarnych2008a",
+		"container-title": "Colloquia Theologica Ottoniana",
+		"issue": "1",
+		"journalAbbreviation": "CTO",
+		"note": "tex.archive-location: Bazhum#530/issues/47620",
+		"page": "57 - 76",
+		"title": "System represji karnych w latach 1945-1956 wobec duchowieństwa Kościoła Katolickiego w Polsce : zbiorowego kustosza tradycji i tożsamości narodowej",
+		"author": [
+			{
+				"family": "Harasimiak",
+				"given": "Grzegorz"
+			}
+		],
+		"issued": {
+			"date-parts": [
+				[
+					"2008"
+				]
+			]
+		}
+	},
+	{
+		"id": "szymanskiJeruzalemJakoMiejsce2008a",
+		"type": "article-journal",
+		"call-number": "ONL CTO 2008/1",
+		"citation-key": "szymanskiJeruzalemJakoMiejsce2008a",
+		"container-title": "Colloquia Theologica Ottoniana",
+		"issue": "1",
+		"journalAbbreviation": "CTO",
+		"note": "tex.archive-location: Bazhum#530/issues/47620",
+		"page": "77 - 93",
+		"title": "Jeruzalem jako miejsce kuszenia Jezusa w świetle Łk 4,9-12",
+		"author": [
+			{
+				"family": "Szymański",
+				"given": "Florencjan"
+			}
+		],
+		"issued": {
+			"date-parts": [
+				[
+					"2008"
+				]
+			]
+		}
+	},
+	{
+		"id": "muchaCoMaWspolnego2008a",
+		"type": "article-journal",
+		"call-number": "ONL CTO 2008/1",
+		"citation-key": "muchaCoMaWspolnego2008a",
+		"container-title": "Colloquia Theologica Ottoniana",
+		"issue": "1",
+		"journalAbbreviation": "CTO",
+		"note": "tex.archive-location: Bazhum#530/issues/47620",
+		"page": "95 - 99",
+		"title": "Co ma wspólnego apokaliptyczny Armagedon z historycznym Megiddo? : Piotrowi Briksowi w odpowiedzi",
+		"author": [
+			{
+				"family": "Mucha",
+				"given": "Paweł Marek"
+			}
+		],
+		"issued": {
+			"date-parts": [
+				[
+					"2008"
+				]
+			]
+		}
+	},
+	{
+		"id": "briksBabilonskiSladWydarzen2008a",
+		"type": "article-journal",
+		"call-number": "ONL CTO 2008/1",
+		"citation-key": "briksBabilonskiSladWydarzen2008a",
+		"container-title": "Colloquia Theologica Ottoniana",
+		"issue": "1",
+		"journalAbbreviation": "CTO",
+		"note": "tex.archive-location: Bazhum#530/issues/47620",
+		"page": "100 - 106",
+		"title": "Babiloński ślad wydarzeń biblijnych? : Komentarz do odkrycia prof. Michaela Jursy",
+		"author": [
+			{
+				"family": "Briks",
+				"given": "Piotr"
+			}
+		],
+		"issued": {
+			"date-parts": [
+				[
+					"2008"
+				]
+			]
+		}
+	},
+	{
+		"id": "piotrbriksSprawozdanieSympozjumMezopotamskie2008a",
+		"type": "article-journal",
+		"call-number": "ONL CTO 2008/1",
+		"citation-key": "piotrbriksSprawozdanieSympozjumMezopotamskie2008a",
+		"container-title": "Colloquia Theologica Ottoniana",
+		"issue": "1",
+		"journalAbbreviation": "CTO",
+		"note": "tex.archive-location: Bazhum#530/issues/47620",
+		"page": "107 - 110",
+		"title": "Sprawozdanie z sympozjum 'Mezopotamskie tło Biblii' : Wydział Teologiczny Uniwersytetu Szczecińskiego, Szczecin, 14-16 kwietnia 2008",
+		"author": [
+			{
+				"family": "Piotr Briks",
+				"given": "Marcin Nockowski"
+			}
+		],
+		"issued": {
+			"date-parts": [
+				[
+					"2008"
+				]
+			]
+		}
+	},
+	{
+		"id": "grabowskiPodrozeMisyjneSwietego2008a",
+		"type": "article-journal",
+		"archive": "Bazhum#530/issues/47637",
+		"call-number": "ONL CTO 2008/2",
+		"citation-key": "grabowskiPodrozeMisyjneSwietego2008a",
+		"container-title": "Colloquia Theologica Ottoniana",
+		"issue": "2",
+		"journalAbbreviation": "CTO",
+		"page": "7 - 21",
+		"title": "Podróże misyjne świętego Pawła",
+		"author": [
+			{
+				"family": "Grabowski",
+				"given": "Tadeusz Zwilnian"
+			}
+		],
+		"issued": {
+			"date-parts": [
+				[
+					"2008"
+				]
+			]
+		}
+	},
+	{
+		"id": "flisPawelNiewolnikiemPanskim2008a",
+		"type": "article-journal",
+		"archive": "Bazhum#530/issues/47637",
+		"call-number": "ONL CTO 2008/2",
+		"citation-key": "flisPawelNiewolnikiemPanskim2008a",
+		"container-title": "Colloquia Theologica Ottoniana",
+		"issue": "2",
+		"journalAbbreviation": "CTO",
+		"page": "23 - 55",
+		"title": "Paweł - niewolnikiem Pańskim",
+		"author": [
+			{
+				"family": "Flis",
+				"given": "Jan"
+			}
+		],
+		"issued": {
+			"date-parts": [
+				[
+					"2008"
+				]
+			]
+		}
+	},
+	{
+		"id": "oczachowskiApostoliPauliOmnia2008a",
+		"type": "article-journal",
+		"archive": "Bazhum#530/issues/47637",
+		"call-number": "ONL CTO 2008/2",
+		"citation-key": "oczachowskiApostoliPauliOmnia2008a",
+		"container-title": "Colloquia Theologica Ottoniana",
+		"issue": "2",
+		"journalAbbreviation": "CTO",
+		"page": "57 - 69",
+		"title": "'Apostoli Pauli omnia et singula' : próba syntezy dzieła św. Pawła",
+		"author": [
+			{
+				"family": "Oczachowski",
+				"given": "Andrzej"
+			}
+		],
+		"issued": {
+			"date-parts": [
+				[
+					"2008"
+				]
+			]
+		}
+	},
+	{
+		"id": "korzecPrzeznaczonyEwangelii2008a",
+		"type": "article-journal",
+		"archive": "Bazhum#530/issues/47637",
+		"call-number": "ONL CTO 2008/2",
+		"citation-key": "korzecPrzeznaczonyEwangelii2008a",
+		"container-title": "Colloquia Theologica Ottoniana",
+		"issue": "2",
+		"journalAbbreviation": "CTO",
+		"page": "71 - 87",
+		"title": "Przeznaczony do Ewangelii",
+		"author": [
+			{
+				"family": "Korzec",
+				"given": "Cezary"
+			}
+		],
+		"issued": {
+			"date-parts": [
+				[
+					"2008"
+				]
+			]
+		}
+	},
+	{
+		"id": "tomaszewskiSensPawlowegoNie2008a",
+		"type": "article-journal",
+		"archive": "Bazhum#530/issues/47637",
+		"call-number": "ONL CTO 2008/2",
+		"citation-key": "tomaszewskiSensPawlowegoNie2008a",
+		"container-title": "Colloquia Theologica Ottoniana",
+		"issue": "2",
+		"journalAbbreviation": "CTO",
+		"page": "89 - 97",
+		"title": "Sens Pawłowego 'Nie wstydzę się Ewangelii' (Rz 1,16a)",
+		"author": [
+			{
+				"family": "Tomaszewski",
+				"given": "Tomasz"
+			}
+		],
+		"issued": {
+			"date-parts": [
+				[
+					"2008"
+				]
+			]
+		}
+	},
+	{
+		"id": "posadzyTloReligijneHistoryczne2008a",
+		"type": "article-journal",
+		"archive": "Bazhum#530/issues/47637",
+		"call-number": "ONL CTO 2008/2",
+		"citation-key": "posadzyTloReligijneHistoryczne2008a",
+		"container-title": "Colloquia Theologica Ottoniana",
+		"issue": "2",
+		"journalAbbreviation": "CTO",
+		"page": "99 - 116",
+		"title": "Tło religijne i historyczne angelologii i demonologii w listach św. Pawła",
+		"author": [
+			{
+				"family": "Posadzy",
+				"given": "Andrzej"
+			}
+		],
+		"issued": {
+			"date-parts": [
+				[
+					"2008"
+				]
+			]
+		}
+	},
+	{
+		"id": "mackowskiPrzywilejPawlowyObowiazujacym2008a",
+		"type": "article-journal",
+		"archive": "Bazhum#530/issues/47637",
+		"call-number": "ONL CTO 2008/2",
+		"citation-key": "mackowskiPrzywilejPawlowyObowiazujacym2008a",
+		"container-title": "Colloquia Theologica Ottoniana",
+		"issue": "2",
+		"journalAbbreviation": "CTO",
+		"page": "117 - 137",
+		"title": "Przywilej Pawłowy w obowiązującym ustawodawstwie Kościoła Katolickiego",
+		"author": [
+			{
+				"family": "Maćkowski",
+				"given": "Andrzej"
+			}
+		],
+		"issued": {
+			"date-parts": [
+				[
+					"2008"
+				]
+			]
+		}
+	},
+	{
+		"id": "goniszewskiSpotkanieSwPawla2008a",
+		"type": "article-journal",
+		"archive": "Bazhum#530/issues/47637",
+		"call-number": "ONL CTO 2008/2",
+		"citation-key": "goniszewskiSpotkanieSwPawla2008a",
+		"container-title": "Colloquia Theologica Ottoniana",
+		"issue": "2",
+		"journalAbbreviation": "CTO",
+		"page": "139 - 147",
+		"title": "Spotkanie św. Pawła z prokonsulem Cypru Sergiuszem Pawłem",
+		"author": [
+			{
+				"family": "Goniszewski",
+				"given": "Piotr"
+			}
+		],
+		"issued": {
+			"date-parts": [
+				[
+					"2008"
+				]
+			]
+		}
+	},
+	{
+		"id": "SlowoWstepne2008",
+		"type": "article-journal",
+		"call-number": "Onl Symp 2008/1 (17)",
+		"citation-key": "SlowoWstepne2008",
+		"container-title": "Sympozjum",
+		"issue": "1",
+		"journalAbbreviation": "Symp",
+		"page": "5",
+		"title": "Słowo wstępne",
+		"volume": "12",
+		"issued": {
+			"date-parts": [
+				[
+					"2008"
+				]
+			]
+		}
+	},
+	{
+		"id": "kaweckiCzyWartoSie2008",
+		"type": "article-journal",
+		"call-number": "Onl Symp 2008/1 (17)",
+		"citation-key": "kaweckiCzyWartoSie2008",
+		"container-title": "Sympozjum",
+		"issue": "1",
+		"journalAbbreviation": "Symp",
+		"page": "7 - 16",
+		"title": "Czy warto się spowiadać? : Trudna odpowiedź w kulturze współczesnej",
+		"volume": "12",
+		"author": [
+			{
+				"family": "Kawecki",
+				"given": "Witold"
+			}
+		],
+		"issued": {
+			"date-parts": [
+				[
+					"2008"
+				]
+			]
+		}
+	},
+	{
+		"id": "fryzelJakUkierunkowacDzisiaj2008",
+		"type": "article-journal",
+		"call-number": "Onl Symp 2008/1 (17)",
+		"citation-key": "fryzelJakUkierunkowacDzisiaj2008",
+		"container-title": "Sympozjum",
+		"issue": "1",
+		"journalAbbreviation": "Symp",
+		"page": "17 - 32",
+		"title": "Jak ukierunkować dzisiaj działania duszpasterskie, aby przywrócić znaczenie i owocność sakramentu pojednania w życiu wiernych",
+		"volume": "12",
+		"author": [
+			{
+				"family": "Fryzel",
+				"given": "Kazimierz"
+			}
+		],
+		"issued": {
+			"date-parts": [
+				[
+					"2008"
+				]
+			]
+		}
+	},
+	{
+		"id": "kowalskiSwiatWspolczesnyPotrzebuje2008",
+		"type": "article-journal",
+		"call-number": "Onl Symp 2008/1 (17)",
+		"citation-key": "kowalskiSwiatWspolczesnyPotrzebuje2008",
+		"container-title": "Sympozjum",
+		"issue": "1",
+		"journalAbbreviation": "Symp",
+		"page": "33 - 56",
+		"title": "Świat współczesny potrzebuje pokuty i nawrócenia",
+		"volume": "12",
+		"author": [
+			{
+				"family": "Kowalski",
+				"given": "Jan"
+			}
+		],
+		"issued": {
+			"date-parts": [
+				[
+					"2008"
+				]
+			]
+		}
+	},
+	{
+		"id": "paluchSzafarzSakramentuPokuty2008",
+		"type": "article-journal",
+		"call-number": "Onl Symp 2008/1 (17)",
+		"citation-key": "paluchSzafarzSakramentuPokuty2008",
+		"container-title": "Sympozjum",
+		"issue": "1",
+		"journalAbbreviation": "Symp",
+		"page": "57 - 70",
+		"title": "Szafarz sakramentu pokuty",
+		"volume": "12",
+		"author": [
+			{
+				"family": "Paluch",
+				"given": "Krzysztof"
+			}
+		],
+		"issued": {
+			"date-parts": [
+				[
+					"2008"
+				]
+			]
+		}
+	},
+	{
+		"id": "ptakSakramentPokutyPojednania2008",
+		"type": "article-journal",
+		"call-number": "Onl Symp 2008/1 (17)",
+		"citation-key": "ptakSakramentPokutyPojednania2008",
+		"container-title": "Sympozjum",
+		"issue": "1",
+		"journalAbbreviation": "Symp",
+		"page": "71 - 80",
+		"title": "Sakrament pokuty i pojednania w ujęciu Katechizmu Kościoła Katolickiego (1992)",
+		"volume": "12",
+		"author": [
+			{
+				"family": "Ptak",
+				"given": "Robert"
+			}
+		],
+		"issued": {
+			"date-parts": [
+				[
+					"2008"
+				]
+			]
+		}
+	},
+	{
+		"id": "kowalskiSamopojednanieOjcowKosciola2008",
+		"type": "article-journal",
+		"call-number": "Onl Symp 2008/1 (17)",
+		"citation-key": "kowalskiSamopojednanieOjcowKosciola2008",
+		"container-title": "Sympozjum",
+		"issue": "1",
+		"journalAbbreviation": "Symp",
+		"page": "81 - 94",
+		"title": "Samopojednanie u Ojców Kościoła Wschodniego",
+		"volume": "12",
+		"author": [
+			{
+				"family": "Kowalski",
+				"given": "Jan"
+			}
+		],
+		"issued": {
+			"date-parts": [
+				[
+					"2008"
+				]
+			]
+		}
+	}
+]
